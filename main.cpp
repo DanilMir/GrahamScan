08717e7da0ed6110ad67fe7bcc99b09faf99abfa
HotBlender <HotBlender@DESKTOP-P7GPNHO>
2021-03-15 20:41:34 +0300

    reading file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <time.h>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 /*структура точки*/
 struct Point {
     double x, y;
-    void print() {
-        cout << x << ' ' << y << '\n';
-    }
 };
 
 /*компаратор для сортировки по углу*/
@@ -31,13 +32,31 @@ bool IsLeft2(Point &a, Point &b, Point &c) {
     return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) >= 0;
 }
 
+template<typename T>
+void insertionSort(vector<T> & data, int n) {
+    int i, j;
+    T tmp;
 
-vector<Point> graham(vector<Point> &p) {
+    for (i = 1; i < n; i++) {
+        j = i;
+        tmp = data[i];
+        while (j > 0 && cmp(tmp, data[j - 1])) {
+            data[j] = data[j - 1];
+            j--;
+        }
+        data[j] = tmp;
+    }
+}
+
+vector<Point> graham(vector<Point> &p, int & iteration) {
     int n = p.size();
+    if(n <= 1)
+        return p;
     auto p0 = p[0];
 
     /* Находим самую самую левую нижнюю точку */
     for (int i = 1; i < n; ++i) {
+        iteration++;
         if (p[i].x < p0.x || (p[i].x == p0.x && p[i].y < p0.y)) {
             p0 = p[i];
         }
@@ -45,13 +64,14 @@ vector<Point> graham(vector<Point> &p) {
 
     /*сделаем точку p0 началом координат*/
     for (auto &i : p) {
+        iteration++;
         i.x -= p0.x;
         i.y -= p0.y;
     }
 
     /*сортируем точки относительно угла*/
-    sort(p.begin(), p.end(), cmp);
 
+    insertionSort(p, n);
 
     /*создаем финальный вектор значений вершин оболочки*/
     vector<Point> st;
@@ -63,6 +83,7 @@ vector<Point> graham(vector<Point> &p) {
         /*удаляем последнюю точку МВО пока она образует невыпуклость*/
         while (!IsLeft(st[st.size() - 2], st[st.size() - 1], p[i])) {
             st.pop_back();
+            iteration++;
         }
         st.push_back(p[i]);
     }
@@ -70,6 +91,7 @@ vector<Point> graham(vector<Point> &p) {
 
     /*возращаем в обычное состояние*/
     for (auto &i : st) {
+        iteration++;
         i.x += p0.x;
         i.y += p0.y;
     }
@@ -78,5 +100,27 @@ vector<Point> graham(vector<Point> &p) {
 
 
 int main() {
+    string inputPath = R"(C:\Users\Danil Mirgayazov\Desktop\temp.txt)"; //файл считывания
+    string outputPath = R"(C:\Users\Danil Mirgayazov\Desktop\output.txt)"; //файл вывода
+    ifstream input(inputPath);
+    ofstream output(outputPath);
 
+    int count;
+    input >> count;
+    for (int i = 0; i < count; ++i) {
+        int n;
+        int iteration = 0;
+        input >> n;
+        vector<Point> p {};
+        for (int j = 0; j < n; ++j) {
+            double x, y;
+            input >> x >> y;
+            Point tt = {x, y};
+            p.push_back(tt);
+        }
+        clock_t start = clock();
+        auto temp = graham(p, iteration);
+        int ms = (clock() - start) / (double) (CLOCKS_PER_SEC / 1000);
+        output << n << ' ' << ms << ' ' << iteration << '\n';
+    }
 }
